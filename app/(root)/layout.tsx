05b597aae1c6faff9ac0dbaaa0cf693bf955ce46
Valentyn Kasyan <vkasyan@gmx.de>
2024-03-07 14:39:24 +0100

    App set-up with (root) rout and public folder contents

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/ui/shared/Header";
+import Footer from "@/components/ui/shared/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
